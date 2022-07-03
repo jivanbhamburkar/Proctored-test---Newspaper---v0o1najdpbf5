95580eb882a7022385aaa57656e4a05c77b361b5
jivanbhamburkar <96575075+jivanbhamburkar@users.noreply.github.com>
2022-07-03 19:10:03 +0530

    Update WeatherComp.jsx

--- src/components/WeatherComp.jsx
+++ src/components/WeatherComp.jsx
@@ -1,61 +1,35 @@
-import React, {useEffect} from 'react'
-
-const WeatherComp = ({tempInfo}) => {
-    const [weatherState, setWeatherState] = React.useState("");
-    const{
-        temp,
-        weathermood,
-        name,
-        country,
-      } = tempInfo;
-
-      useEffect(() =>{
-          if(weathermood){
-              switch(weathermood){
-                  case "Clouds": setWeatherState("wi-day-cloudy");
-                  break;
-                  case "Haze": setWeatherState("wi-fog");
-                  break;
-                  case "Clear": setWeatherState("wi-day-sunny");
-                  break;
-                  case "Mist": setWeatherState("wi-dust");
-                  break;
-                  case "Rain": setWeatherState("wi-rain");
-                  break;
-                  default:
-                      setWeatherState("wi-day-sunny");
-                      break;
-              }
-          }
-      },[weathermood])
-      
-     
+import React, { useEffect, useState } from "react";
+const apiKey = "b7f172124e075c9c4c942b337794bef2";
+const WeatherComp = () => {
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitue] = useState();
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      const lol = navigator.geolocation.getCurrentPosition((pos) => {
+        setLatitue(pos.coords.longitude);
+        setLongitude(pos.coords.longitude);
+      });
+    }
+  };
+  const fetchNews = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,dail&appid=${apiKey}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  useEffect(() => {
+    fetchNews();
+  }, []);
   return (
-    <>
-     {/*our temp card*/}
-     <article className='widget'>
-      <div className='weatherIcon'>
-      <i className={`wi ${weatherState}`}></i>
-        </div>
-
-      <div className='weatherInfo'>
-        <div className='temperature'>
-          <span>{Math.round(temp)}&deg;</span>
-        </div>
-
-        <div className='description'>
-          <div className='weatherCondition'>{weathermood}</div>
-          <div className='place'>{name}, {country}</div>
-        </div>
-      </div>
-      <div className='date'> {new Date().toDateString()} </div>
-
-       
-       
-    </article>
-      
-    </>
-  )
-}
+    <div>
+      <button onClick={getLocation}>lol</button>
+    </div>
+  );
+};
 
 export default WeatherComp;
